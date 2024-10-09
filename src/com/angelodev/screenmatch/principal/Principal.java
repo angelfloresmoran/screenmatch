44067044c7ef585c0239ed514c27007237516e81
Luis Angel Flores <angel.steph.3006@gmail.com>
2024-10-09 00:57:50 -0600

    Se agrega la carpeta principal al paquete del proyecto

--- src/com/angelodev/screenmatch/principal/Principal.java
+++ src/com/angelodev/screenmatch/principal/Principal.java
@@ -1,13 +1,16 @@
+package com.angelodev.screenmatch.principal;
+
 import com.angelodev.screenmatch.calculos.CalculadoraDeTiempo;
 import com.angelodev.screenmatch.calculos.FiltroRecomendacion;
 import com.angelodev.screenmatch.modelos.Episodio;
 import com.angelodev.screenmatch.modelos.Pelicula;
 import com.angelodev.screenmatch.modelos.Serie;
+
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args){
-        Pelicula miPelicula = new Pelicula();
-        miPelicula.setNombre("Encanto");
-        miPelicula.setFechaDeLanzamiento(2021);
+        Pelicula miPelicula = new Pelicula("Encanto", 2021);
         miPelicula.setDuracionEnMinutos(190);
         miPelicula.setIncluidoEnElPlan(true);
         miPelicula.evalua(10);
@@ -20,9 +23,7 @@ public class Principal {
         System.out.println("La media es: " + miPelicula.calculaMedia());
         System.out.println("******************");
 
-        Serie casaDragon = new Serie();
-        casaDragon.setNombre("La casa del dragon");
-        casaDragon.setFechaDeLanzamiento(2022);
+        Serie casaDragon = new Serie("La casa del dragon", 2022);
         casaDragon.setTemporadas(1);
         casaDragon.setMinutosPorEpisodio(50);
         casaDragon.setEpisodiosPorTemporada(10);
@@ -30,9 +31,7 @@ public class Principal {
         System.out.println("---");
         System.out.println("Duración de toda la serie en minutos: " + casaDragon.getDuracionEnMinutos());
 
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.setNombre("Avengers");
-        otraPelicula.setFechaDeLanzamiento(2012);
+        Pelicula otraPelicula = new Pelicula("Avengers", 2012);
         otraPelicula.setDuracionEnMinutos(180);
         System.out.println("\n********************************");
         otraPelicula.muestraFichaTecnica();
@@ -55,6 +54,23 @@ public class Principal {
         episodio.setSerie(casaDragon);
         episodio.setTotalDeVisualizaciones(101);
         filtroRecomendacion.filtra(episodio);
+
+        //Pelicula (esta palabra "Pelicula" es una variable de referencia y se puede sustituit por "var") peliculaDeAngel = new Pelicula (este es el tipo de dato()
+        var peliculaDeAngel = new Pelicula("El señor de los anillos", 2001);
+        //peliculaDeAngel.setNombre("El señor de los Anillos");
+        peliculaDeAngel.setDuracionEnMinutos(180);
+
+        ArrayList<Pelicula> listaDePeliculas = new ArrayList<>();
+        listaDePeliculas.add(peliculaDeAngel);
+        listaDePeliculas.add(miPelicula);
+        listaDePeliculas.add(otraPelicula);
+        System.out.println("Tamaño de la lista: " + listaDePeliculas.size());
+        System.out.println("La primera pelicula es: " + listaDePeliculas.get(0).getNombre());
+
+        System.out.println(listaDePeliculas);
+        System.out.println("toString de la pelicula: " + listaDePeliculas.get(0).toString());
+
+
 
     }
 }
